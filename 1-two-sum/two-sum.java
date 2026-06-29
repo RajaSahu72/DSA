5634a5ce461742348f1817b6cfdaa4cf84dfbbf9
Raja Kumar Sahu <hey.rajasahu@gmail.com>
2026-06-29 19:12:37 +0530

    Time: 2 ms (99.37%) | Memory: 47.3 MB (22.98%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -5,12 +5,14 @@ class Solution {
 
         for(int i = 0; i<nums.length; i++){
             int first = nums[i];
+            // Find the second num needed to make the target
             int second = target - first;
 
             if(map.containsKey(second)){
+                // map.get(sec) -> idx of second , i -> idx of first
                 return new int[] {map.get(second),i};
             }
-
+            // if sec num not in the map than put the curr el in the map
             map.put(nums[i],i);
         }
 
