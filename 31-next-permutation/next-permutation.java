a29be96c9e9c4aae11c8964d9a815cf9ead4946b
Raja Kumar Sahu <hey.rajasahu@gmail.com>
2026-07-12 20:24:34 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (15.98%) - LeetSync

---
+++ 31-next-permutation/next-permutation.java
@@ -0,0 +1,48 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        // Step 1: Find the pivot 
+        int pivot = -1;
+        int n = nums.length;
+
+        for(int i = n-2; i>=0; i--){
+            if(nums[i] < nums[i+1]){
+                pivot = i;
+                break;
+            }
+        }
+
+        // If no pivot is found, reverse the whole array
+        if(pivot == -1){
+            reverse(nums,0,n-1);
+            return;
+        }
+
+        //Step 2: Find the next larger element and swap
+        for(int i = n-1; i>pivot; i--){
+            if(nums[i] > nums[pivot]){
+                swap(nums,i,pivot);
+                break;
+            }
+        }
+
+        // Step 3: reverse(piv+1 to n-1)
+        reverse(nums,pivot+1,n-1);
+    }
+
+    private void swap(int nums[], int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private void reverse(int[] nums,int st, int end){
+        while(st < end){
+            int temp = nums[st];
+            nums[st] = nums[end];
+            nums[end] = temp;
+
+            st++;
+            end--;
+        }
+    }
+}
