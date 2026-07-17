5259bd3eaf826aaefd2d14f86cbf576c4580ddd2
Raja Kumar Sahu <hey.rajasahu@gmail.com>
2026-07-17 10:49:55 +0530

    Time: 30 ms (88.65%) | Memory: 59.3 MB (45.83%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -5,6 +5,7 @@ class Solution {
 
         for(int i = 0; i<nums.length; i++){
 
+            // Skip duplicate first elements
             if(i>0 && nums[i] == nums[i-1]) continue;
 
             // Two Pointers
@@ -24,10 +25,12 @@ class Solution {
                     ans.add(Arrays.asList(nums[i],nums[j],nums[k]));
                     j++; k--;
 
+                    // Skip duplicates for j
                     while(j<k && nums[j] == nums[j-1]){
                         j++;
                     }
 
+                    // Skip duplicates for k
                     while(j<k && nums[k] == nums[k+1]){
                         k--;
                     }
