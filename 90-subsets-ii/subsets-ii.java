3dc992fb811f47f40d4391b147bf78ab91133147
Raja Kumar Sahu <hey.rajasahu@gmail.com>
2026-07-28 18:23:12 +0530

    Time: 2 ms (99.70%) | Memory: 45.2 MB (24.54%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        getAllSubsets(nums, new ArrayList<>(), 0, ans);
+        return ans;
+    }
+
+    private void getAllSubsets(int nums[], List<Integer> curr, int idx, List<List<Integer>> ans){
+
+        // Basecase
+        if(idx == nums.length){
+            ans.add(new ArrayList<>(curr));
+            return;
+        }
+
+        // Include curr el
+        curr.add(nums[idx]);
+        getAllSubsets(nums, curr, idx+1, ans);
+
+        // Backtrack (remove last el added)
+        curr.remove(curr.size()-1);
+
+        // Skipping duplicate values
+        while(idx+1 < nums.length && nums[idx] == nums[idx+1]) {
+            idx++;
+        }
+
+        //Exclude curr el
+         getAllSubsets(nums, curr, idx+1, ans);
+    }
+
+}
