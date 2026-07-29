bf2db0e6d81cc64c1f4b7b3765bc7e17f40507e2
Raja Kumar Sahu <hey.rajasahu@gmail.com>
2026-07-29 11:35:12 +0530

    Time: 3 ms (91.36%) | Memory: 46.7 MB (44.28%) - LeetSync

--- 47-permutations-ii/permutations-ii.java
+++ 47-permutations-ii/permutations-ii.java
@@ -2,7 +2,7 @@ class Solution {
     List<List<Integer>> res = new ArrayList<>();
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<Integer> list = new ArrayList<>();
-        Arrays.sort(nums);
+        Arrays.sort(nums); // sort 
         boolean vis[] = new boolean[nums.length];
         getPerms(nums, vis, new ArrayList<>());
         return res;
@@ -10,14 +10,16 @@ class Solution {
 
    private void getPerms(int nums[], boolean vis[], List<Integer> list) {
 
+        // Base case 
         if (list.size() == nums.length) {
             res.add(new ArrayList<>(list)); 
             return;
         }
 
         for (int i = 0; i < nums.length; i++) {
-            if (vis[i]) continue;
+            if (vis[i]) continue; // skip already used el
 
+            // skip duplicate permutations
             if(i>0 && nums[i] == nums[i-1] && !vis[i-1]) continue;
 
             list.add(nums[i]);
