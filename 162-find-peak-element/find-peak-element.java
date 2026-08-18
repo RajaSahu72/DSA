357ea5ce0d0edf6a7bd0b7a19ebe99f34ccea83f
Raja Kumar Sahu <hey.rajasahu@gmail.com>
2026-08-18 20:50:35 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (57.59%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -6,8 +6,8 @@ class Solution {
         if(nums[0] > nums[1]) return 0;
         if(nums[n-1] > nums[n-2]) return n-1;
 
-        int low = 1;
-        int high = n-2;
+        int low = 1; // we checked for arr[0] thats why we intialize l = 1
+        int high = n-2; // we checked for arr[0] thats why we intialize h = n-2
 
         while(low <= high){
             int mid = low + (high-low)/2;
@@ -15,11 +15,11 @@ class Solution {
             if(nums[mid-1] < nums[mid] && nums[mid] > nums[mid+1]){
                 return mid;
             }
-            else if(nums[mid-1] < nums[mid]){
-                low = mid+1;
+            else if(nums[mid-1] < nums[mid]){ 
+                low = mid+1; // peek el in right
             }
             else{
-                high = mid-1;
+                high = mid-1; // peek el in left
             }
         }
         return -1;
