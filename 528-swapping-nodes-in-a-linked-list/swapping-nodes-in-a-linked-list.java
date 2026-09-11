3ab94c306c3c381645dcdb102c0f8912be4fb2de
Raja Kumar Sahu <hey.rajasahu@gmail.com>
2026-09-11 19:08:47 +0530

    Time: 2 ms (50.52%) | Memory: 120.1 MB (7.14%) - LeetSync

--- 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.java
+++ 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.java
@@ -10,23 +10,28 @@
  */
 class Solution {
     public ListNode swapNodes(ListNode head, int k) {
-        ListNode prev = head;
+
+        // Step 1 - Find length of the LL
+        ListNode curr = head;
         int size = 0;
-        while(prev != null){
-            prev = prev.next;
+        while(curr != null){
+            curr = curr.next;
             size++;
         }
 
+        // Step 2 - Find kth node from begining
         ListNode first = head;
         for(int i = 1; i<k; i++){
             first = first.next;
         }
 
+        // Step 3 - Find kth node from end
         ListNode second = head;
         for(int i = 1; i<size-k+1; i++){
             second = second.next;
         }
 
+        // Step 4 - Swap values
         int temp = first.val;
         first.val = second.val;
         second.val = temp;
