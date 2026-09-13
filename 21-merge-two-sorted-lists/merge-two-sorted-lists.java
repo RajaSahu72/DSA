4cab510b808a4a276603e22d1266a6eccdf9f65f
Raja Kumar Sahu <hey.rajasahu@gmail.com>
2026-09-13 20:03:56 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (19.91%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -14,7 +14,7 @@ class Solution {
         ListNode temp = dummy;
 
         while(l1 != null && l2 != null){
-            if(l1.val <= l2.val){
+            if(l1.val < l2.val){
                 temp.next = l1;
                 l1 = l1.next;
             }
@@ -23,17 +23,15 @@ class Solution {
                 l2 = l2.next;
             }
             temp = temp.next;
-
         }
 
         if(l1 != null){
             temp.next = l1;
         }
-        else{
+        if(l2 != null){
             temp.next = l2;
         }
 
         return dummy.next;
-
     }
 }
